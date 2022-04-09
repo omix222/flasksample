6ac2247f208803b902fa69343fe10b2ccb3eb923
Hiromi Takahashi <hiromix222@gmail.com>
2022-04-10 08:26:06 +0900

    render_template追加

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +8,11 @@ def index():
 @app.route('/next')
 def hoge():
     return 'NextPage!!'
+@app.route('/temp')
+def hello():
+    name = "Hoge"
+    #return name
+    return render_template('hello.html', title='flask test', name=name)
 
 if __name__ == '__main__':
     app.debug = True
