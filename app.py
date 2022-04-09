f641e7c4b5d8d6c273c8f45b42b21d53a798b172
Hiromi Takahashi <hiromix222@gmail.com>
2022-04-10 08:54:30 +0900

    use db

--- app.py
+++ app.py
@@ -1,18 +1,46 @@
 from flask import Flask, render_template
+import pymysql
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello Flask!'
+
 # デコレータでURLマッピングする
 @app.route('/next')
 def hoge():
     return 'NextPage!!'
+
+# render を使ってテンプレートファイルを利用
 @app.route('/temp')
 def hello():
     name = "Hoge"
     #return name
     return render_template('hello.html', title='flask test', name=name)
+
+@app.route('/member')
+def getmember():
+
+    #db setting
+    db = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='root',
+            db='testdb',
+            charset='utf8',
+            cursorclass=pymysql.cursors.DictCursor,
+        )
+
+    cur = db.cursor()
+    sql = "select * from members"
+    cur.execute(sql)
+    members = cur.fetchall()
+
+    cur.close()
+    db.close()
+
+    #return name
+    return render_template('member.html', title='flask test', members=members) #変更
 
 if __name__ == '__main__':
     app.debug = True
--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@
 
 ## 実行方法
 python3 app.py
+
+## 参考メモ
+https://qiita.com/zaburo/items/5091041a5afb2a7dffc8
